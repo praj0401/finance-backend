9fe6a85fa28fb733cd2881b7ea57b755d6fa2ea3
Your Name <your@email.com>
2026-04-06 19:53:51 +0530

    fix startup db creation

--- main.py
+++ main.py
@@ -2,10 +2,16 @@ from fastapi import FastAPI
 from database import Base, engine
 from routers import users, records, dashboard
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI(title="Finance Backend API")
 
+
+# ✅ Create tables on startup (important for deployment)
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
+
+
+# ✅ Include routers
 app.include_router(users.router)
 app.include_router(records.router)
 app.include_router(dashboard.router)
